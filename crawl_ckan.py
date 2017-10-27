a07224514c03cb6a29b028950966dbcc5b5a736c
Jason Koh <bk7749@gmail.com>
2017-10-27 13:07:53 -0700

    gMerge branch 'master' of https://github.com/jbkoh/open-data-profile

--- crawl_ckan.py
+++ crawl_ckan.py
@@ -1,12 +1,14 @@
 import json
 import requests
+import pdb
 
 
 #baseurls = ['https://data.boston.gov/api/3'
 #           ]
 
 baseurl = 'https://data.boston.gov/api/3'
-packages = requests.get(baseurl + '/action/package_list_with_resources', 
-                        params={'limit':200}).json()['result']
-dataset = packages[0]
-print(dataset)
+data = requests.get(baseurl + '/action/current_package_list_with_resources',
+                        params={'limit':10000}).json()['result']
+
+with open('result/boston.json', 'w') as fp:
+    json.dump(data, fp)
--- profile_socrata.py
+++ profile_socrata.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import urlparse
 
 with open('result/socrata.json', 'r') as fp:
     data = json.load(fp)
@@ -10,5 +11,12 @@ for datum in data:
     if 'lacity' in datum['link']:
         la_data.append(datum)
 
+urls = set()
+for datum in data:
+    url = datum['link']
+    urls.add(urlparse(url).hostname)
+
 with open('result/la.json', 'w') as fp:
     json.dump(la_data, fp, indent=2)
+
+print('# of Hosts: ', len(urls))
